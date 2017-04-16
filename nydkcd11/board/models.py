6afd0f538c696660954f6ced22a52e41ae71b957
Brian Lee <blee025432@gmail.com>
2017-04-16 18:05:25 -0400

    board models set up

---
+++ nydkcd11/board/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Person(models.Model):
+	name = models.CharField(max_length = 50)
+	school = models.CharField(max_length = 75)
+	position = models.CharField(max_length = 50)
+	desc = models.CharField(max_length = 300)
+	image = models.ImageField()
+
