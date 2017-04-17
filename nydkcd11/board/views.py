7a0b255183aa8cc8fae8d503228429ad56b7b3da
Brian Lee <blee025432@gmail.com>
2017-04-17 17:42:47 -0400

    first template

--- nydkcd11/board/views.py
+++ nydkcd11/board/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
+from django.template import loader
 # Create your views here.
-
 def index(request):
-	return HttpResponse("Hello, you're at the divisonal board page")
+	names_list = Person.objects.all()
+	template = loader.get_template('board/index.html')
+	context = {
+		'names_list' : names_list	
+	}
+	return HttpResponse(template.render(context, request))
+def results(request, person_id): #temp
+	obj = Person.objects.get(id=person_id)
+	output = ""
+	output += obj.name + "\n" + obj.school + "\n" + obj.desc
+	return HttpResponse(output)
