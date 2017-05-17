a0695caf8c71e48a7193cf2417e74031604e703e
Brian Lee <blee025432@gmail.com>
2017-05-17 01:10:11 -0400

    set up 404?

--- nydkcd11/board/views.py
+++ nydkcd11/board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Person
 from django.template import loader
 # Create your views here.
@@ -11,8 +11,21 @@ def index(request):
 	}
 	#return HttpResponse(template.render(context, request))
 	return render(request, 'board/index.html', context)
-def results(request, person_id): #temp
-	obj = Person.objects.get(id=person_id)
-	output = ""
-	output += obj.name + "\n" + obj.school + "\n" + obj.desc
-	return HttpResponse(output)
+def results(request, person_id): #temp, some kind of 404 thing going?
+	try:
+		obj = Person.objects.get(id=person_id)
+		output = ""
+		output += obj.name + "\n" + obj.school + "\n" + obj.desc
+		return HttpResponse(output)
+	except Person.DoesNotExist:
+		raise Http404("Not found")
+	return render(request,"board/detail.html",{'person':person})
+'''
+def details(request, person_id):
+	try:
+		person = Person.objects.get(pk=question_id)
+	except Person.DoesNotExist:
+		raise Http404("The person you queried isn't part of the board.")
+	return render(request,"board/detail.html",{'person':person})
+'''
+
