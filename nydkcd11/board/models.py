2c64650ffc0e5884f107fb9770ca592d034501a2
Brian Lee <blee025432@gmail.com>
2017-06-04 17:25:34 -0400

    started blog

--- nydkcd11/board/models.py
+++ nydkcd11/board/models.py
@@ -6,7 +6,7 @@ class Person(models.Model):
 	name = models.CharField(max_length = 50)
 	school = models.CharField(max_length = 75)
 	position = models.CharField(max_length = 50)
-	desc = models.CharField(max_length = 300)
+	desc = models.CharField(max_length = 3000)
 	image = models.ImageField(upload_to='pictures/')
 	#addd sub functions from shell
 	def __str__(self):
