c6b6ed915f8fa7206c22fb688757e5b8e2d36ec7
Brian Lee <blee025432@gmail.com>
2017-06-04 19:44:55 -0400

    setting up test post

--- nydkcd11/blog/models.py
+++ nydkcd11/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Post(models.Model):
+	title = models.CharField(max_length = 100)
+	author = models.CharField(max_length = 50)
+	pub_date = models.DateTimeField('date publshed')
+	body = models.CharField(max_length = 10000)
+
+class Image(models.Model):
+	post = models.ForeignKey(Post, on_delete = models.CASCADE)
+	desc = models.CharField(max_length = 1000)
+	#image =  models.ImageField()
+
+class Video(models.Model):
+	post = models.ForeignKey(Post, on_delete = models.CASCADE)
+	url = models.CharField(max_length = 1000)
+
+class Tag(models.Model):
+	post = models.ForeignKey(Post, on_delete = models.CASCADE)
+	name = models.CharField(max_length = 30)
 # Create your models here.
