5b8965db5160ed32b7a85f8a4fe4274a58b827e8
Brian Lee <blee025432@gmail.com>
2017-06-05 21:59:39 -0400

    basic blog structure set up

--- nydkcd11/blog/views.py
+++ nydkcd11/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Post
 # Create your views here.
 def index(request):
-	return HttpResponse("you are at the blog page")
+	latest_post_list = Post.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('blog/index.html')
+	context = {
+		'latest_post_list': latest_post_list,	
+	}
+	return HttpResponse(template.render(context,request))
+def detail(request, post_id):
+	return HttpResponse("this is post %s." % post_id)
+
