5a74d99cb16b8603e43920aa71b45f8d74107db2
Brian Lee <blee025432@gmail.com>
2017-06-05 22:29:11 -0400

    working through 404

--- nydkcd11/blog/views.py
+++ nydkcd11/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Post
 # Create your views here.
@@ -9,7 +9,15 @@ def index(request):
 	context = {
 		'latest_post_list': latest_post_list,	
 	}
-	return HttpResponse(template.render(context,request))
+	#return HttpResponse(template.render(context,request))
+	return render(request, 'blog/index.html',context)
 def detail(request, post_id):
-	return HttpResponse("this is post %s." % post_id)
-
+	'''
+	try:
+		post = Post.objects.get(pk=post_id)
+	except Post.DoesNotExist:
+		raise Http404("Post doesn't exist")
+	return render(request, 'blog/detail.html',{'post' : post})
+	'''
+	post = get_object_or_404(Post, pk = post_id)
+	return render(request, 'blog/index.html',{'post':post})
