0f1f0d7ee099c8c75a57ce52819344aef448f2b2
Brian Lee <blee025432@gmail.com>
2017-06-08 01:27:05 -0400

    started model setup

--- nydkcd11/contact/models.py
+++ nydkcd11/contact/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+class Contact:
+	name = models.CharField(max_length = 30)
+	school = models.CharField(max_length = 50)
+	message = models.CharField
 # Create your models here.
