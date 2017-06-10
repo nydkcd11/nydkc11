a66835aae5e51f4b268376a932caf1222e159ea4
Brian Lee <blee025432@gmail.com>
2017-06-10 00:45:41 -0400

    freeze work on forms

--- nydkcd11/contact/views.py
+++ nydkcd11/contact/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("contact page")
+	return HttpResponse("contact page: need to set up form, but databases already set up")
 # Create your views here.
