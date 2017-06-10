e9010a9b503aefd01628bee226427e89c2cbb49d
Brian Lee <blee025432@gmail.com>
2017-06-10 00:30:00 -0400

    basic database setup

--- nydkcd11/contact/models.py
+++ nydkcd11/contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Contact(models.Model):
+	name=models.CharField(max_length=50)
+	school = models.CharField(max_length=50)
+	email=models.CharField(max_length=50)
+	title=models.CharField(max_length=50)
+	text=models.TextField()
+	def __str__(self):
+		return self.title
 # Create your models here.
--- nydkcd11/nydkcd11/settings.py
+++ nydkcd11/nydkcd11/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'contact.apps.ContactConfig',
 	'blog.apps.BlogConfig',
 	'board.apps.BoardConfig',
     'django.contrib.admin',
