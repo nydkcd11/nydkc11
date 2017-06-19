776567fb09bbdcf6551c8e2c95ecc141da87fc24
Brian Lee <blee025432@gmail.com>
2017-06-19 16:48:02 -0400

    added new signup view

--- nydkcd11/forms/views.py
+++ nydkcd11/forms/views.py
@@ -8,5 +8,7 @@ def papers(request):
 	papers_list = Papers.objects.all()
 	return render(request,'forms/papers.html',{'papers_list':papers_list})
 def signup(request):
+	link_list = Link.objects.all()
+	return render(request, 'forms/signup.html',{'link_list':link_list})
 	return HttpResponse("this is the official signup page")
 # Create your views here.
