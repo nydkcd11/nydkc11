8a0eab7bf05f85e0537d3b25d15b8d736caf6c17
Brian Lee <blee025432@gmail.com>
2017-06-19 10:54:00 -0400

    added newsletter and minutes

--- nydkcd11/resources/admin.py
+++ nydkcd11/resources/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Newsletter, Minutes
+admin.site.register(Newsletter)
+admin.site.register(Minutes)
 # Register your models here.
--- nydkcd11/resources/models.py
+++ nydkcd11/resources/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+class Newsletter(models.Model):
+	month = models.DateField()
+	url = models.CharField(max_length=100)
+class Minutes(models.Model):
+	month = models.DateField()
+	url = models.CharField(max_length=100)
 # Create your models here.
