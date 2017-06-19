e9399a456cc0787d9060a73b0ec06a9a7d70b024
Brian Lee <blee025432@gmail.com>
2017-06-19 19:20:01 -0400

    fixing minutes switch case

--- nydkcd11/resources/models.py
+++ nydkcd11/resources/models.py
@@ -7,6 +7,48 @@ class Newsletter(models.Model):
 class Minutes(models.Model):
 	month = models.DateField()
 	url = models.CharField(max_length=100)
+	def january():
+		return "January"
+	def february():
+		return "February"
+	def march():
+		return "March"
+	def april():
+		return "April"
+	def may():
+		return "May"
+	def june():
+		return "June"
+	def july():
+		return "July"
+	def august():
+		return "August"
+	def september():
+		return "September"
+	def october():
+		return "October"
+	def november():
+		return "November"
+	def december():
+		return "December"
 	def __str__(self):
-		return str(self.month)
+		options = {
+			1:"January",
+			2:"february",
+			3:"march",
+			4:"april",
+			5:"may",
+			6:"june",
+			7:"july",
+			8:"august",
+			9:"september",
+			10:"october",
+			11:"november",
+			12:"december",	
+		}
+		number = self.month.month
+		return options[number]()
+
+	
+	
 # Create your models here.
--- nydkcd11/resources/views.py
+++ nydkcd11/resources/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Video
+from .models import Newsletter, Minutes
 def index(request):
 	video_list = Video.objects.all()
 	context = {
@@ -14,7 +15,9 @@ def videos(request):
 	}
 	return render(request,'resources/videos.html',context)
 def newsletter(request):
-	return HttpResponse("newsletter")
+	newsletter_list = Newsletter.objects.all()
+	return render(request,'resources/newsletter.html',{'newsletter_list':newsletter_list})
 def minutes(request):
-	return HttpResponse("minutes")
+	minutes_list = Minutes.objects.all()
+	return render(request,'resources/minutes.html',{'minutes_list':minutes_list})
 # Create your views here.
