ecf7fadc757eedcd2f471e466693b79da191bf91
Brian Lee <blee025432@gmail.com>
2017-06-19 16:43:16 -0400

    setup paper view

--- nydkcd11/forms/views.py
+++ nydkcd11/forms/views.py
@@ -5,7 +5,8 @@ from blog.models import Link
 def index(request):
 	return HttpResponse("this is the forms page")
 def papers(request):
-	return HttpResponse("this is the official paperwork page")
+	papers_list = Papers.objects.all()
+	return render(request,'forms/papers.html',{'papers_list':papers_list})
 def signup(request):
 	return HttpResponse("this is the official signup page")
 # Create your views here.
