07c61ff929966d5e5e4e8f8eab61a86821ffc720
Brian Lee <blee025432@gmail.com>
2017-06-20 15:14:39 -0400

    created homepage

--- nydkcd11/nydkcd11/urls.py
+++ nydkcd11/nydkcd11/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
+	url(r'^$', views.index, name = 'index'),
 	url(r'^about/', include('about.urls')),
     url(r'^admin/', admin.site.urls),
 	url(r'^blog/', include('blog.urls')),
