ba192c726481921d151642b9b48d5a6845f7ecf7
Brian Lee <blee025432@gmail.com>
2017-06-28 10:16:03 -0400

    proper view sort

--- nydkcd11/about/views.py
+++ nydkcd11/about/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Division, School, FAQ
 def division(request):
-	member_list = Division.objects.all()
+	member_list = Division.objects.order_by('-pk').reverse()
 	return render(request, 'about/division.html',{'member_list':member_list,})
 def clubs(request):
-	school_list = School.objects.all()
+	school_list = School.objects.order_by('-pk').reverse()
 	return render(request, 'about/school.html',{'school_list':school_list})
 def faq(request):
-	faq_list = FAQ.objects.all()
+	faq_list = FAQ.objects.order_by('-pk').reverse()
 	return render(request, 'about/faq.html',{'faq_list':faq_list})
 # Create your views here.
