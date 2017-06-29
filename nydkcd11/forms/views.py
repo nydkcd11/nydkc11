1a26b961a5b3f58ef7946da4db6ec2311159d7c8
Brian Lee <blee025432@gmail.com>
2017-06-29 11:07:43 -0400

    ordered links properly

--- nydkcd11/forms/views.py
+++ nydkcd11/forms/views.py
@@ -6,6 +6,6 @@ def papers(request):
 	papers_list = Papers.objects.order_by('-pk').reverse()
 	return render(request,'forms/papers.html',{'papers_list':papers_list})
 def signup(request):
-	link_list = Link.objects.order_by('-pk').reverse()
+	link_list = Link.objects.order_by('-link')
 	return render(request, 'forms/signup.html',{'link_list':link_list})
 # Create your views here.
