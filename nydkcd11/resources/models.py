f4c3571aa46d42cdd87fad40dee30b7cda129944
Brian Lee <blee025432@gmail.com>
2017-07-02 18:37:58 -0400

    paired newsletter with post system

--- nydkcd11/resources/models.py
+++ nydkcd11/resources/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from blog.models import Post
 class Newsletter(models.Model):
+	post = models.ForeignKey(Post, on_delete = models.CASCADE)
 	month = models.CharField(max_length = 30)
 	url = models.CharField(max_length=100)
 	def __str__(self):
