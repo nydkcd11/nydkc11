929ae124ef76c5a973fa249870fe878070c6ebee
Brian Lee <blee025432@gmail.com>
2017-07-05 23:56:01 -0400

    added more post desc

--- nydkcd11/blog/admin.py
+++ nydkcd11/blog/admin.py
@@ -9,7 +9,9 @@ class ArticleAdmin(admin.ModelAdmin):
 	list_display = ('title','author','date')
 class LinkAdmin(admin.ModelAdmin):
 	list_display=('name','host','event_choices')
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','author','pub_date')
+admin.site.register(Post, PostAdmin)
 admin.site.register(Video)
 admin.site.register(Link, LinkAdmin)
 admin.site.register(Image, ImageAdmin)
