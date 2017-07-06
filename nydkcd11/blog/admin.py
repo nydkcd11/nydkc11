e512d4d4bc1e08ca5264b7a836306134d2c870d7
Brian Lee <blee025432@gmail.com>
2017-07-05 23:51:48 -0400

    more admin updating

--- nydkcd11/blog/admin.py
+++ nydkcd11/blog/admin.py
@@ -5,9 +5,13 @@ class ImageAdmin(admin.ModelAdmin):
 	ordering = ('-post',)
 	def pub_date(self, obj):
 		return obj.post.pub_date
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title','author','date')
+class LinkAdmin(admin.ModelAdmin):
+	list_display=('name','host','event_choices')
 admin.site.register(Post)
 admin.site.register(Video)
-admin.site.register(Link)
+admin.site.register(Link, LinkAdmin)
 admin.site.register(Image, ImageAdmin)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 # Register your models here.
