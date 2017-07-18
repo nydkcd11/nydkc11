c15138de202b47e5ae4186de3eb0e26dea542ef9
Brian Lee <blee025432@gmail.com>
2017-07-18 18:15:26 -0400

    set up models

--- nydkcd11/projects/models.py
+++ nydkcd11/projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from embed_video.fields import EmbedVideoField
+class Level(models.Model):
+	key_level = models.CharField('Level of Key Club?', max_length = 100)
+	name = models.CharField('Name of Project', max_length=100)
+	logo = models.ImageField(upload_to='projects/')
+	bkgnd_photo = models.ImageField(upload_to='projects/')
+	def_short = RichTextField()
+	video = EmbedVideoField()
+	extend_desc = RichTextField()
+	fundrs_goal = models.IntegerField()
+	hours_goal = models.IntegerField()
+	def __str__(self):
+		return self.key_level
 # Create your models here.
--- nydkcd11/projects/views.py
+++ nydkcd11/projects/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Level
 def index(request):
 	return HttpResponse("response")
+def detail(request, level_id):
+	project = get_object_or_404(Level, pk=level_id)
+	return HttpResponse("test")
 # Create your views here.
