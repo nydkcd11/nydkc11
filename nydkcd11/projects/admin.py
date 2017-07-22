e53a27bd225e6e02b8d5dff5787a806c00697b1e
Brian Lee <blee025432@gmail.com>
2017-07-22 09:43:22 -0400

    added admin details

--- nydkcd11/projects/admin.py
+++ nydkcd11/projects/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Level
-admin.site.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+	list_display = ('key_level','name')	
+admin.site.register(Level, LevelAdmin)
+
 # Register your models here.
