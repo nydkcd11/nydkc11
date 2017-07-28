2b31191edd2956d47981b0e1ccbe84d83afb6d45
Brian Lee <blee025432@gmail.com>
2017-07-27 22:26:46 -0400

    updated admin

--- nydkcd11/events/admin.py
+++ nydkcd11/events/admin.py
@@ -1,9 +1,14 @@
+from embed_video.admin import AdminVideoMixin
 from django.contrib import admin
 from .models import DTC, List, Part, Convention
 class ListAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug':('name',),}
+class ConventionAdmin(AdminVideoMixin, admin.ModelAdmin):
+	pass
+class PartAdmin(admin.ModelAdmin):
+	list_display = ('header', 'convention')	
 admin.site.register(DTC)
 admin.site.register(List, ListAdmin)
-admin.site.register(Part)
-admin.site.register(Convention)
+admin.site.register(Part, PartAdmin)
+admin.site.register(Convention, ConventionAdmin)
 # Register your models here.
