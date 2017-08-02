4a8751454e973fd269c4320a5a93275ec7c15acc
Brian Lee <blee025432@gmail.com>
2017-08-02 18:57:53 -0400

    homepage changed

--- nydkcd11/nydkcd11/views.py
+++ nydkcd11/nydkcd11/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import Image, Post, Article
 from contact.forms import EmailForm
+from events.models import Service
+from django.utils import timezone
+from datetime import timedelta
 def index(request):
 	image_master = Image.objects.order_by('-post') #need way to sort by pub_date from post
 	image_list = []
@@ -13,5 +16,8 @@ def index(request):
 	for i in range(len(image_list)):
 		length.append(i)
 	blog_list = Post.objects.order_by('pub_date_2').reverse()[:4]
-	article_list = Article.objects.order_by('-pk')[:4]
+	#article_list = Article.objects.order_by('-pk')[:4]
+	start_date = timezone.now().date()
+	end_date = start_date + timedelta(days=365)
+	article_list = Service.objects.filter(start_time__range=(start_date,end_date))[0:4]
 	return render(request, 'nydkcd11/home.html',{'image_list':image_list,'length':length, 'blog_list':blog_list,'article_list':article_list})
