72af410464d39f3f3bca6870a3cfca2f32e59039
Brian Lee <blee025432@gmail.com>
2017-08-03 16:56:42 -0400

    removed newsletter view and template

--- nydkcd11/resources/urls.py
+++ nydkcd11/resources/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'resources'
 urlpatterns = [
 	url(r'^videos/$',views.videos,name='videos'),
-	url(r'^newsletter/$',views.newsletter,name='newsletter'),
 	url(r'^minutes/$', views.minutes,name='minutes'),
 ]
--- nydkcd11/resources/views.py
+++ nydkcd11/resources/views.py
@@ -8,9 +8,6 @@ def videos(request):
 		'video_list':video_list,	
 	}
 	return render(request,'resources/videos.html',context)
-def newsletter(request):
-	newsletter_list = Newsletter.objects.order_by('-pk').reverse()
-	return render(request,'resources/newsletter.html',{'newsletter_list':newsletter_list})
 def minutes(request):
 	minutes_list = Minutes.objects.order_by('-pk').reverse()
 	return render(request,'resources/minutes.html',{'minutes_list':minutes_list})
