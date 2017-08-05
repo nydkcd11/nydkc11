633ad0769e993787a6567918df262b1be0736435
Brian Lee <blee025432@gmail.com>
2017-08-05 18:31:42 -0400

    paginate videos

--- nydkcd11/resources/views.py
+++ nydkcd11/resources/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Video
 from .models import Newsletter, Minutes
+from blog.page import page
 def videos(request):
-	video_list = Video.objects.order_by('-post')
+	video_list = page(Video.objects.order_by('-post'),request)
 	context = {
 		'video_list':video_list,	
 	}
