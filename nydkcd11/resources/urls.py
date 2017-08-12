738f4169655de94d99ae931bee8fca74a1b344aa
Brian Lee <blee025432@gmail.com>
2017-08-12 13:37:57 -0400

    added gallery

--- nydkcd11/resources/urls.py
+++ nydkcd11/resources/urls.py
@@ -5,4 +5,6 @@ app_name = 'resources'
 urlpatterns = [
 	url(r'^videos/$',views.videos,name='videos'),
 	url(r'^minutes/$', views.minutes,name='minutes'),
+	url(r'^gallery/$', views.gallery, name = 'gallery'),
+	url(r'^gallery/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$', views.gallery_specific, name = 'gallery_specific')
 ]
--- nydkcd11/resources/views.py
+++ nydkcd11/resources/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from blog.models import Video
+from blog.models import Video, Image
 from .models import Newsletter, Minutes
 from blog.page import page
 def videos(request):
@@ -12,4 +12,40 @@ def videos(request):
 def minutes(request):
 	minutes_list = Minutes.objects.order_by('-pk').reverse()
 	return render(request,'resources/minutes.html',{'minutes_list':minutes_list})
+def month_parse(num):
+	month_dict = {
+		1:'January',
+		2:'February',
+		3:'March',
+		4:'April',	
+		5:'May',	
+		6:'June',	
+		7:'July',	
+		8:'August',	
+		9:'September',	
+		10:'October',	
+		11:'November',	
+		12:'December',	
+	}
+	return month_dict[int(num)]
+def month_gen(year):
+	images = Image.objects.filter(post__pub_date_2__year = year)
+	month_groups = {}
+	for i in range(1,12):
+		month_groups[i] = Image.objects.filter(post__pub_date_2__month = i)
+	return month_groups
+def gallery(request):
+	images = Image.objects.order_by('-post')
+	images_grouped = {}
+	for image in images:
+		try:
+			images_grouped[image.post.pub_date_2.year]
+		except KeyError:
+			images_grouped[image.post.pub_date_2.year] = month_gen(image.post.pub_date_2.year)	
+	return render(request,'resources/gallery.html',{'images_grouped':images_grouped})
+def gallery_specific(request, year, month):
+	images = Image.objects.filter(post__pub_date_2__year = year).filter(post__pub_date_2__month = month)
+	if len(images) == 0:
+		return redirect('resources:gallery')
+	return render(request, 'resources/gallery_specific.html', {'images':images, 'year':year,'month':month_parse(month)})
 # Create your views here.
