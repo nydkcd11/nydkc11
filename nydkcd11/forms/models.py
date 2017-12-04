6b441a3e3c5656494fc1b72c9407fce984c6e1d0
Brian Lee <blee025432@gmail.com>
2017-12-03 22:29:40 -0500

    added paper

--- nydkcd11/forms/models.py
+++ nydkcd11/forms/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+#Resources (e.g. the MRF Form)
+#don't ask why this has its own table
 class Papers(models.Model):
-	name = models.CharField(max_length = 30)
-	url = models.CharField(max_length = 300) 
+	name = models.CharField(max_length = 30,verbose_name="Title of Paper/Form/Application")
+	url = models.CharField(max_length = 300,verbose_name="URL to Paper/Form/Application") 
 	def __str__(self):
 		return self.name
+	class Meta:
+		verbose_name = "Application Resource"
+		verbose_name = "Application Resources"
 # Create your models here.
