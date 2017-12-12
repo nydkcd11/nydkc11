fc9a7f66c9ae332ae2d42dadae7b3014a9574baa
Brian Lee <blee025432@gmail.com>
2017-12-11 22:26:22 -0500

    apps commenting

--- nydkcd11/about/apps.py
+++ nydkcd11/about/apps.py
@@ -1,5 +1,7 @@
+#App data for the About App
+#Chances are you won't ever need to touch apps.py files (besides initial setup)
 from django.apps import AppConfig
 
-
+#holds the internal name of the app (for Django usage)
 class AboutConfig(AppConfig):
     name = 'about'
